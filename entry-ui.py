c86e5995e92b196917a73949ec59825f01ad4fde
pregus101 <159173502+pregus101@users.noreply.github.com>
2026-02-25 17:05:44 -0800

    moving some stuff around

--- entry-ui.py
+++ entry-ui.py
@@ -19,6 +19,8 @@ from wave_renderer import WaveVisualizer
 from queue_handler import shuffler
 from queue_handler import generated_unshuffled_queue
 from Song_Bar import SongBar
+from input_handler import previous
+from input_handler import skip
 import time
 import random
 
@@ -225,60 +227,11 @@ while running:
 
                 # Check if skip button was clicked
                 if (SCREEN_WIDTH-SCREEN_WIDTH/5)/2+30+SCREEN_WIDTH/5 <= mouse_pos[0] <= (SCREEN_WIDTH-SCREEN_WIDTH/5)/2+80+SCREEN_WIDTH/5 and SCREEN_HEIGHT-50 <= mouse_pos[1] <= SCREEN_HEIGHT-30 and visualizer_running:
-                    STARTED = True
-                    play_pause = "play"
-                    play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
-                    pygame.mixer.music.stop()
-
+                    STARTED, play_pause, play_pause_button_path = skip()
 
                 # Check if previous button was clicked
                 if (SCREEN_WIDTH-SCREEN_WIDTH/5)/2-80+SCREEN_WIDTH/5 <= mouse_pos[0] <= (SCREEN_WIDTH-SCREEN_WIDTH/5)/2-30+SCREEN_WIDTH/5 and SCREEN_HEIGHT-50 <= mouse_pos[1] <= SCREEN_HEIGHT-30 and visualizer_running:
-                    
-                    if current_time_sec != None and current_time_sec <= 10:
-                        try:
-                            file_path = os.path.join(currently_playing_folder_path, list(played_songs.keys())[-1])
-                            skip = False 
-                            play_pause = "play"
-                        except:
-                            skip = True
-                        
-                        if not skip:
-                            STARTED = True
-                            PLAYING_SONG = os.path.basename(file_path)
-
-                            queue_raw.insert(played_songs[PLAYING_SONG], PLAYING_SONG)  # Add current song back to the front of the queue_raw
-                            queue.insert(0, PLAYING_SONG)
-                            played_songs.pop(PLAYING_SONG, None) # Remove the song from played_songs dictionary
-                                
-                            # Get album cover art for the selected track
-                            render_size, cover_art_path = get_cover_art(file_path, SIZE)
-
-                            # CREATE AND START WAVE VISUALIZER
-                            visualizer = WaveVisualizer(file_path, 
-                                                        render_size[0], 
-                                                        render_size[1])
-                            # Set wave color to contrast with album cover
-                            visualizer.set_color_from_image(cover_art_path)
-                            visualizer.load_audio()
-                            visualizer.play()
-                            visualizer_running = True
-
-                            # Load the sound file as a Sound object to get its length
-                            temp_sound_object = pygame.mixer.Sound(file_path)
-                            total_length = temp_sound_object.get_length() # Length in seconds
-                            play_pause = "play"  # Ensure play/pause state is set to "play" when previous button is clicked
-                            play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
-
-                    else:
-                        # CREATE AND START WAVE VISUALIZER
-                        visualizer = WaveVisualizer(file_path, 
-                                                    render_size[0], 
-                                                    render_size[1])
-                        visualizer.load_audio()
-                        visualizer.play()
-                        visualizer_running = True
-                        play_pause = "play"  # Ensure play/pause state is set to "play" when previous button is clicked
-                        play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
+                    play_pause, played_songs, queue_raw, queue, play_pause_button_path, visualizer, STARTED, PLAYING_SONG, render_size, cover_art_path = previous(current_time_sec, currently_playing_folder_path, played_songs, queue_raw, queue, SIZE, visualizer, screen, total_length, play_pause, play_pause_button_path, visualizer_running, STARTED, PLAYING_SONG, render_size, cover_art_path, file_path)
 
                 # Check if pause/play button was clicked
                 if (SCREEN_WIDTH-SCREEN_WIDTH/5)/2-25+SCREEN_WIDTH/5 <= mouse_pos[0] <= (SCREEN_WIDTH-SCREEN_WIDTH/5)/2+25+SCREEN_WIDTH/5 and SCREEN_HEIGHT-75 <= mouse_pos[1] <= SCREEN_HEIGHT-25:
@@ -470,51 +423,11 @@ while running:
                         STARTED = False
                     play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Default image for pause button
 
-        if input_key == Key.media_next and STARTED:
-            pygame.mixer.music.stop()
+        if input_key == Key.media_next and visualizer_running:
+            STARTED, play_pause, play_pause_button_path = skip()
 
         if input_key == Key.media_previous and STARTED:
-            if current_time_sec != None and current_time_sec <= 10:
-                try:
-                    file_path = os.path.join(currently_playing_folder_path, list(played_songs.keys())[-1])
-                    skip = False 
-                except:
-                    skip = True
-                
-                if not skip:
-                    STARTED = True
-                    PLAYING_SONG = os.path.basename(file_path)
-
-                    queue_raw.insert(played_songs[PLAYING_SONG], PLAYING_SONG)  # Add current song back to the front of the queue_raw
-                    queue.insert(0, PLAYING_SONG)
-                    played_songs.pop(PLAYING_SONG, None) # Remove the song from played_songs dictionary
-                        
-                    # Get album cover art for the selected track
-                    render_size, cover_art_path = get_cover_art(file_path, SIZE)
-
-                    # CREATE AND START WAVE VISUALIZER
-                    visualizer = WaveVisualizer(file_path, 
-                                                render_size[0], 
-                                                render_size[1])
-                    # Set wave color to contrast with album cover
-                    visualizer.set_color_from_image(cover_art_path)
-                    visualizer.load_audio()
-                    visualizer.play()
-                    visualizer_running = True
-
-                    # Load the sound file as a Sound object to get its length
-                    temp_sound_object = pygame.mixer.Sound(file_path)
-                    total_length = temp_sound_object.get_length() # Length in seconds
-
-            else:
-                # CREATE AND START WAVE VISUALIZER
-                visualizer = WaveVisualizer(file_path, 
-                                            render_size[0], 
-                                            render_size[1])
-                visualizer.load_audio()
-                visualizer.play()
-                visualizer_running = True
-        
+            play_pause, played_songs, queue_raw, queue, play_pause_button_path, visualizer, STARTED, PLAYING_SONG, render_size, cover_art_path = previous(current_time_sec, currently_playing_folder_path, played_songs, queue_raw, queue, SIZE, visualizer, screen, total_length, play_pause, play_pause_button_path, visualizer_running, STARTED, PLAYING_SONG, render_size, cover_art_path, file_path) 
 
     # ========================================================================
     # AUTO-PLAY & QUEUE MANAGEMENT
---
+++ input_handler.py
@@ -0,0 +1,59 @@
+import pygame
+from wave_renderer import WaveVisualizer
+from get_metadata import get_cover_art
+import os
+
+def skip():
+    STARTED = True
+    play_pause = "play"
+    play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
+    pygame.mixer.music.stop()  # Stop current music to allow next song to play immediately
+    return STARTED, play_pause, play_pause_button_path
+
+def previous(current_time_sec, currently_playing_folder_path, played_songs, queue_raw, queue, SIZE, visualizer, screen, total_length, play_pause, play_pause_button_path, visualizer_running, STARTED, PLAYING_SONG, render_size, cover_art_path, file_path):
+    if current_time_sec != None and current_time_sec <= 10:
+        try:
+            file_path = os.path.join(currently_playing_folder_path, list(played_songs.keys())[-1])
+            skip = False 
+            play_pause = "play"
+        except:
+            skip = True
+        
+        if not skip:
+            STARTED = True
+            PLAYING_SONG = os.path.basename(file_path)
+
+            queue_raw.insert(played_songs[PLAYING_SONG], PLAYING_SONG)  # Add current song back to the front of the queue_raw
+            queue.insert(0, PLAYING_SONG)
+            played_songs.pop(PLAYING_SONG, None) # Remove the song from played_songs dictionary
+                
+            # Get album cover art for the selected track
+            render_size, cover_art_path = get_cover_art(file_path, SIZE)
+
+            # CREATE AND START WAVE VISUALIZER
+            visualizer = WaveVisualizer(file_path, 
+                                        render_size[0], 
+                                        render_size[1])
+            # Set wave color to contrast with album cover
+            visualizer.set_color_from_image(cover_art_path)
+            visualizer.load_audio()
+            visualizer.play()
+            visualizer_running = True
+
+            # Load the sound file as a Sound object to get its length
+            temp_sound_object = pygame.mixer.Sound(file_path)
+            total_length = temp_sound_object.get_length() # Length in seconds
+            play_pause = "play"  # Ensure play/pause state is set to "play" when previous button is clicked
+            play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
+
+    else:
+        # CREATE AND START WAVE VISUALIZER
+        visualizer = WaveVisualizer(file_path, 
+                                    render_size[0], 
+                                    render_size[1])
+        visualizer.load_audio()
+        visualizer.play()
+        visualizer_running = True
+        play_pause = "play"  # Ensure play/pause state is set to "play" when previous button is clicked
+        play_pause_button_path = os.path.join(os.path.dirname(__file__), "assets/play_pause.jpg")  # Change play/pause button to playing state when a new song is selected
+    return play_pause, played_songs, queue_raw, queue, play_pause_button_path, visualizer, STARTED, PLAYING_SONG, render_size, cover_art_path
