7b4507e581e1fc85bfca8f472226b11393ba952f
pregus101 <pfirsideking@gmail.com>
2026-04-23 12:52:41 -0700

    Some more java practice

--- FileAndDir.java
+++ FileAndDir.java
@@ -1,25 +1,67 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.file;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class FileAndDir {
     public static void main(String[] args) {
-        getFiles(System.getProperty("user.home"));
-        getDirs(System.getProperty("user.home"));
+        System.out.println(getFiles(System.getProperty("user.home")));
+        System.out.println(getDirs(System.getProperty("user.home")));
     }
 
     public static ArrayList<Path> getFiles(String dir){
-        ArrayList<Path> files = new ArrayList<Path>();
-        return files;
+        Path dirPath = Paths.get(dir);
+        try (Stream<Path> stream = Files.list(dirPath)){
+            Map<Boolean, List<Path>> partitioned = stream.collect(
+                Collectors.partitioningBy(Files::isDirectory)
+            );
+            return new ArrayList<Path>(partitioned.get(false));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<Path>();
+        }
     }
 
     public static ArrayList<Path> getDirs(String dir){
-        ArrayList<Path> dirs = new ArrayList<Path>();
-        return dirs;
+        Path dirPath = Paths.get(dir);
+        try (Stream<Path> stream = Files.list(dirPath)){
+            Map<Boolean, List<Path>> partitioned = stream.collect(
+                Collectors.partitioningBy(Files::isDirectory)
+            );
+            return new ArrayList<Path>(partitioned.get(true));
+        } 
+        catch (IOException e) {
+                e.printStackTrace();
+                return new ArrayList<Path>();
+            }
+        
+    }
+
+    public static ArrayList<Path> getFiles(Path dirPath){
+        try (Stream<Path> stream = Files.list(dirPath)){
+            Map<Boolean, List<Path>> partitioned = stream.collect(
+                Collectors.partitioningBy(Files::isDirectory)
+            );
+            return new ArrayList<Path>(partitioned.get(false));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<Path>();
+        }
+    }
+
+    public static ArrayList<Path> getDirs(Path dirPath){
+        try (Stream<Path> stream = Files.list(dirPath)){
+            Map<Boolean, List<Path>> partitioned = stream.collect(
+                Collectors.partitioningBy(Files::isDirectory)
+            );
+            return new ArrayList<Path>(partitioned.get(true));
+        } 
+        catch (IOException e) {
+                e.printStackTrace();
+                return new ArrayList<Path>();
+            }
+        
     }
 }
--- player.java
+++ player.java
@@ -1,12 +1,15 @@
 
-
-
-import javazoom.jl.player.Player;
-import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class player {
-    public player(ArrayList<String>){
-        
+    public String playing_song = "";
+    public Path current_dir;
+    private ArrayList<Path> unshuffled;
+    public ArrayList<Path> queue;
+    private boolean shuffled = false;
+    public player(Path current_dir){
+        this.current_dir = current_dir;
+        unshuffled = FileAndDir.getFiles(current_dir);
     }
-
 }
